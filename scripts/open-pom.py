a547543f3a42f88d7a2830730d0fb0f2e80448a0
Rafael Ponte <rafael.ponte@zup.com.br>
2022-11-04 15:33:32 -0300

    Ignores DynamoDB Repositories when scanning for JPA Repositories;

--- scripts/open-pom.py
+++ scripts/open-pom.py
@@ -2,13 +2,17 @@ import xml.etree.ElementTree as ET
 from templateframework.metadata import Metadata
 
 def run(metadata: Metadata = None):
+    
     tree = ET.parse('pom.xml')
     root = tree.getroot()
+
     for xml in root:
         if "groupId" in xml.tag:
             group = xml.tag
         if "artifactId" in xml.tag:
             artifact = xml.tag
+        if "name" in xml.tag:
+            name = xml.tag
 
     group_id = root.find(group)
 
@@ -18,13 +22,19 @@ def run(metadata: Metadata = None):
     else:  
         artifact_id_text = artifact_id.text 
 
+    project_name = root.find(name)
+    project_name_text = project_name.text.replace("-", "").title()
+
     package = f"{group_id.text}.{artifact_id.text}"
     path_code_directory = f"src.main.java.{group_id.text}.{artifact_id_text}"
     path_test_code_directory = f"src.test.java.{group_id.text}.{artifact_id_text}"
+    full_path_main_java_folder = path_code_directory.replace(".", "/")
+    application_class_file_name = f"{project_name_text}Application.java"
     
     metadata.inputs['application_package'] = package
     metadata.inputs['application_package_folder'] = package.replace(".", "/")
-    metadata.inputs['main_code_directory'] = path_code_directory
-    metadata.inputs['test_code_directory'] = path_test_code_directory
+    metadata.inputs['full_path_main_java_folder'] = path_code_directory.replace(".", "/")
+    metadata.inputs['full_path_test_java_folder'] = path_test_code_directory.replace(".", "/")
+    metadata.inputs['application_class_source_file_path'] = f"{full_path_main_java_folder}/{application_class_file_name}"
 
     return metadata
